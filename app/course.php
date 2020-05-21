8ed654c9e98e1e8b3d2b3ffc596b40731925a47a
Aly Mostafa <34445689+Alymostafa@users.noreply.github.com>
2020-05-21 22:50:17 +0200

    Create course.php

---
+++ app/course.php
@@ -0,0 +1,39 @@
+ 
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Course extends Model
+{
+    protected $fillable = [
+        'name','code','min_students_number','department_id','semester','credit_hours',
+    ];
+    public function studentCourses()
+    {
+        return $this->hasMany('App\StudentCourse');
+    }
+    public function students()
+    {
+        return $this->hasManyThrough('App\Student','App\StudentCourse');
+    }
+    public function studentGrades()
+    {
+        return $this->hasMany('App\StudentGrade');
+    }
+    public function department()
+    {
+        return $this->belongsTo('App\Department');
+    }
+    public function courseHalls()
+    {
+        return $this->hasMany('App\CourseHall');
+    }
+    public function halls() 
+    {
+        return $this->hasManyThrough('App\Hall','App\CourseHall');
+    }
+    
+
+}
