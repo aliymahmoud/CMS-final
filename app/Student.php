c568aacec05a0ccdf3ef9f6b4ff8ef08111c79d1
alishraf12 <49539488+alishraf12@users.noreply.github.com>
2020-05-21 22:56:28 +0200

    Create Student.php

---
+++ app/Student.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Student extends Model
+{
+    protected $fillable = [
+        'user_id','unique_id','level','department_id','gpa',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+    public function studentCourses()
+    {
+        return $this->hasMany('App\StudentCourse');
+    }
+    public function courses()
+    {
+        return $this->hasManyThrough('App\Course','App\StudentCourse');
+    }
+    public function department()
+    {
+        return $this->belongsTo('App\Department');
+    }
+}
