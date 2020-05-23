6cd0108340b4f51ecc7de1ce5026e2eea3315ab4
alishraf12 <49539488+alishraf12@users.noreply.github.com>
2020-05-23 04:18:12 +0200

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Contracts\Events\Dispatcher;
+use JeroenNoten\LaravelAdminLte\Events\BuildingMenu;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,8 +23,83 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(Dispatcher $event)
     {
-        //
+        $event->listen(BuildingMenu::class, function (BuildingMenu $event) {
+        $event->menu->add(
+
+        );
+        if(\Auth::check()){
+          if(\Auth::user()->role == 0){
+            $event->menu->add(['header' => 'Admin']);
+            $event->menu->add([
+                    'text'    => 'Courses',
+                    'icon'    => 'fas fa-fw fa-share',
+                    'submenu' => [
+                        [
+                            'text' => 'Add New Course',
+                            'url'  =>  route('add.course'),
+                        ],
+                        
+                        [
+                            'text' => 'All Courses',
+                            'url'  => route('list.course'),
+                        ],
+                    ],
+                ],
+                [
+                    'text'    => 'Halls',
+                    'icon'    => 'fas fa-fw fa-share',
+                    'submenu' => [
+                        [
+                            'text' => 'level_one',
+                            'url'  => '#',
+                        ],
+                        
+                        [
+                            'text' => 'level_one',
+                            'url'  => '#',
+                        ],
+                    
+                    ],
+                ],
+                [
+                    'text'    => 'Instructors',
+                    'icon'    => 'fas fa-fw fa-share',
+                    'submenu' => [
+                        [
+                            'text' => 'level_one',
+                            'url'  => '#',
+                        ],
+                        
+                        [
+                            'text' => 'level_one',
+                            'url'  => '#',
+                        ],
+                    
+                    ],
+                ],
+                [
+                    'text'    => 'Students',
+                    'icon'    => 'fas fa-fw fa-share',
+                    'submenu' => [
+                        [
+                            'text' => 'Add New Student',
+                            'url'  => route('add.student'),
+                        ],
+                        [
+                            'text' => 'All Students',
+                            'url' => route('list.student'),
+                        ],
+                    
+                    ],
+                ]
+             );
+          }
+          else {
+            $event->menu->add(['header' => 'Courses']);
+          }
+        }
+      });
     }
 }
