41e9e0557d27246dc429f2701a8a6a85c44b25ad
Ali Mahmoud <aliy.mahmoud00@gmail.com>
2020-06-03 23:06:50 +0200

    editing admin controller

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -9,12 +9,6 @@ use App\Department;
 
 class AdminController extends Controller
 {
-      public function getListStudents(Request $request)
-    {
-        $level = $request->level;
-        $students = Student::where('level',$level)->get();
-        return view('admin.student.list',compact('students','level'));
-    }
     
      public function getAddStudent(Request $request)
     {
