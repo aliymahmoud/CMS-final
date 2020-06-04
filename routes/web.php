2567a5e12c20d9a1c947173a581f465649ce588e
Ahmed-Reda99 <62517416+Ahmed-Reda99@users.noreply.github.com>
2020-06-04 23:39:43 +0200

    fixing routes

--- routes/web.php
+++ routes/web.php
@@ -12,11 +12,62 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('login');
 });
+
+Route::get('/get/{folderA}/{folderB}','ImageController@get');
+Route::post('/post','ImageController@post')->name('post');
 Auth::routes([
     'register' => false,
 ]);
 Route::group(['middleware' => ['auth'],],function(){
+
+    Route::get('/home',function(){
+        return view('home');
+    })->name('home')->middleware('student');
+
+    Route::get('/profile','StudentController@profile')->name('profile');
         
+    // ADMIN FUNCTIONS
+    Route::group(['prefix' => 'admin','middleware' => ['admin'],],function()
+    {
+        // students
+        Route::get('/add-student','AdminController@getAddStudent')->name('add.student');
+        Route::post('/add-student','AdminController@postAddStudent')->name('add.student');
+        Route::get('/edit-student/{name}', 'AdminController@getUpdateStudent')->name('edit.student');
+        Route::post('/update-student/{name}', 'AdminController@postUpdateStudent')->name('update.student');
+        Route::get('/students','AdminController@getListStudents')->name('list.student');
+        Route::get('/student/{name}', 'AdminController@getViewStudent')->name('view.student');
+
+        // Courses
+        Route::get('/add-course','AdminController@getAddCourse')->name('add.course');
+        Route::post('/add-course','AdminController@postAddCourse')->name('add.course');
+        Route::get('/courses', 'AdminController@getListCourses')->name('list.course');
+        Route::get('/edit-course/{code}', 'AdminController@getUpdateCourse')->name('edit.course');
+        Route::post('/update-course/{code}', 'AdminController@postUpdateCourse')->name('update.course');
+        Route::get('/course/{code}', 'AdminController@postUpdateCourse')->name('view.course');
+
+        // halls
+        Route::get('/halls','AdminController@listHalls')->name('list.hall');
+        Route::get('/add-hall','AdminController@createHall')->name('add.hall');
+        Route::post('/add-hall','AdminController@storeHall')->name('add.hall');
+        Route::get('/hall/{id}','AdminController@showHall')->name('update.hall');
+        Route::get('/edit-hall/{id}','AdminController@editHall')->name('edit.hall');
+        Route::post('/update-hall/{id}','AdminController@updateHall')->name('update.hall');
+
+        // instructors
+        Route::get('/instructors','AdminController@listInstructors')->name('list.instructor');
+        Route::get('/add-instructor','AdminController@createInstructor')->name('add.instructor');
+        Route::post('/add-instructor','AdminController@storeInstructor')->name('add.instructor');
+        Route::get('/instructor/{id}','AdminController@showInstructor')->name('update.instructor');
+        Route::get('/edit-instructor/{id}','AdminController@editInstructor')->name('edit.instructor');
+        Route::post('/update-instructor/{id}','AdminController@updateInstructor')->name('update.instructor');
+    });   
+    
+    // STUDENT FUNCTIONS
+    Route::post('first-login','StudentController@firstLogin')->name('first-login');
+    Route::group(['middleware' => ['student'],],function(){
+        Route::post('update-profile','StudentController@updateProfile')->name('update.profile');
+    });
 });
+        
