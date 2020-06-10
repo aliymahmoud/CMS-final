2e356b73d1e97e0ce35fb9f5b3b30a32bce24048
alishraf12 <49539488+alishraf12@users.noreply.github.com>
2020-06-11 00:21:42 +0200

    Update create.blade.php

--- resources/views/admin/student/create.blade.php
+++ resources/views/admin/student/create.blade.php
@@ -12,31 +12,31 @@
               <div class="box-body">
                 <div class="form-group">
                   <label>First name</label>
-                  <input type="text" class="form-control" placeholder="Enter First Name" name="firstName">
+                  <input type="text" class="form-control" placeholder="Enter First Name" name="firstName" value="{{ old("firstName") }}">
                 </div>
                 <div class="form-group">
                   <label>Last name</label>
-                  <input type="text" class="form-control" placeholder="Enter Last Name" name="lastName">
+                  <input type="text" class="form-control" placeholder="Enter Last Name" name="lastName" value="{{ old("lastName") }}">
                 </div>
-                <div class="form-group">
+                {{-- <div class="form-group">
                   <label>User name</label>
                   <input type="text" class="form-control" placeholder="Enter User Name" name="userName">
-                </div>
-                <div class="form-group">
+                </div> --}}
+                {{-- <div class="form-group">
                   <label>Email</label>
                   <input type="email" class="form-control" placeholder="Enter Email" name="email">
-                </div>
-                <div class="form-group">
+                </div> --}}
+                {{-- <div class="form-group">
                   <label>Password</label>
                   <input type="password" class="form-control" placeholder="Enter Password" name="password">
-                </div>
+                </div> --}}
                 <div class="form-group">
                   <label for="male">Male</label>
                   <input type="radio" name="gender" value="1" id="male">
                   <label for="female">Female</label>
                   <input type="radio" name="gender" value="0" id="female">
                 </div>
-                <div class="form-group">
+                {{-- <div class="form-group">
                   <label >Department</label>
                   <select class ="form-control" name="department_id">
                     <option selected disabled>Select Department</option>
@@ -45,15 +45,15 @@
                     <option value="2">Information Systems</option>
                     <option value="3">Information Technology</option>
                   </select>
-                </div>
-                <div class="form-group">
+                </div> --}}
+                {{-- <div class="form-group">
                   <label>Level</label>
                   <input type="number" class="form-control" placeholder="Enter Level" name="level">
-                </div>
-                <div class="form-group">
+                </div> --}}
+                {{-- <div class="form-group">
                   <label>GPA</label>
                   <input type="number" step="0.01" ste class="form-control" placeholder="Enter GPA" name="gpa">
-                </div>
+                </div> --}}
               </div>
               <!-- /.box-body -->
 
